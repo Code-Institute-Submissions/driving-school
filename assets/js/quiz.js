79a4899044a32390c511d22640e67434129bc38f
Gary Barrett <itsgaz@gmail.com>
2021-04-27 22:53:28 +0000

    js: add shuffle function and declare consts for dom elements

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,2 +1,17 @@
+let questions = shuffle(questionData).slice(0, 10);
+
+// Shuffles the questions
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
+}
+
+
 // Start the quiz
-//startQuiz();
+startQuiz();
