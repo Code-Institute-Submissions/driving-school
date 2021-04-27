51e4daff10a2fda1ea941d904364b8c30d8f752d
Gary Barrett <itsgaz@gmail.com>
2021-04-27 22:43:42 +0000

    js: add first set of questions

---
+++ assets/js/questions.js
@@ -0,0 +1,276 @@
+let questionData = [{
+        id: 0,
+        question: "What is the effect of an incorrectly adjusted driver's seat?",
+        answers: [{
+                text: " It can delay the driver from operating a control",
+                correct: true,
+                answered: false
+            },
+            {
+                text: "It improves the driver's ability to operate the clutch",
+                correct: false,
+                answered: false
+            },
+            {
+                text: "It enables the driver to relax more on a long journey",
+                correct: false,
+                answered: false
+            },
+            {
+                text: "It gives the driver a better view in the rear-view mirror",
+                correct: false,
+                answered: false
+            }
+        ]
+    },
+    {
+        id: 1,
+        question: "What type of noise might fast cornering create?",
+        answers: [{
+                text: "Brake squeal",
+                correct: false,
+                answered: false
+            },
+            {
+                text: "Tyre squeal",
+                correct: true,
+                answered: false
+            },
+            {
+                text: "Exhaust backfire",
+                correct: false,
+                answered: false
+            },
+            {
+                text: "Engine backfire",
+                correct: false,
+                answered: false
+            }
+        ]
+    },
+    {
+        id: 2,
+        question: "If the brake pedal of a vehicle feels soft or spongy when applied, what does this mean?",
+        answers: [{
+                text: "Worn brake pads",
+                correct: false,
+                answered: false
+            },
+            {
+                text: "A fault in the hand-brake cable",
+                correct: false,
+                answered: false
+            },
+            {
+                text: "The parking brake is on",
+                correct: false,
+                answered: false
+            },
+            {
+                text: " A fault in the brake fluid system",
+                correct: true,
+                answered: false
+            }
+        ]
+    },
+    {
+        id: 3,
+        question: "What is the recommended method of driving a car with automatic transmission?",
+        answers: [{
+                text: "Operate the accelerator and brake with the right foot",
+                correct: true,
+                answered: false
+            },
+            {
+                text: "Operate the accelerator with the right foot and the brake with the left foot",
+                correct: false,
+                answered: false
+            },
+            {
+                text: "Operate the accelerator and brake with the left foot",
+                correct: false,
+                answered: false
+            },
+            {
+                text: "Use the handbrake to bring the vehicle to a halt",
+                correct: false,
+                answered: false
+            }
+        ]
+    },
+    {
+        id: 4,
+        question: "Which vehicle is exempt from speed limits when being used in an emergency?",
+        answers: [{
+                text: "Mountain rescue",
+                correct: false,
+                answered: false
+            },
+            {
+                text: "Doctor on call",
+                correct: false,
+                answered: false
+            },
+            {
+                text: "Fire brigade",
+                correct: true,
+                answered: false
+            },
+            {
+                text: "Breakdown recovery",
+                correct: false,
+                answered: false
+            }
+        ]
+    },
+    {
+        id: 5,
+        question: "What is the maximum gross weight of a vehicle that may be driven by the holder of a Category B driving licence?",
+        answers: [{
+                text: "3,250 kilograms",
+                correct: false,
+                answered: false
+            },
+            {
+                text: "3,500 kilograms",
+                correct: true,
+                answered: false
+            },
+            {
+                text: "3,000 kilograms",
+                correct: false,
+                answered: false
+            },
+            {
+                text: "3,450 kilograms",
+                correct: false,
+                answered: false
+            }
+        ]
+    },
+    {
+        id: 6,
+        question: "What is the minimum legal tread depth for tyres on cars?",
+        answers: [{
+                text: " 2.6 millimetres",
+                correct: false,
+                answered: false
+            },
+            {
+                text: "1 millimetre",
+                correct: false,
+                answered: false
+            },
+            {
+                text: "1.6 millimetres",
+                correct: true,
+                answered: false
+            },
+            {
+                text: "0.6 millimetres",
+                correct: false,
+                answered: false
+            }
+        ]
+    },
+    {
+        id: 7,
+        question: "What does the load index of a tyre indicate?",
+        answers: [{
+                text: "The maximum load of the vehicle",
+                correct: false,
+                answered: false
+            },
+            {
+                text: " The maximum air pressure of the tyre.",
+                correct: false,
+                answered: false
+            },
+            {
+                text: "The maximum load per axle.",
+                correct: false,
+                answered: false
+            },
+            {
+                text: "The maximum load a tyre can carry.",
+                correct: true,
+                answered: false
+            }
+        ]
+    },
+    {
+        id: 8,
+        question: "What should a driver do when involved in an incident where nobody is injured but the vehicles are causing a danger or obstruction on the road?",
+        answers: [{
+                text: "Move all passengers and put a red warning triangle on the road.",
+                correct: false,
+                answered: false
+            },
+            {
+                text: "Take care when moving damaged vehicles and make every effort to warn other traffic.",
+                correct: true,
+                answered: false
+            },
+            {
+                text: "Halt all traffic until it has been established who is at fault.",
+                correct: false,
+                answered: false
+            },
+            {
+                text: "Wait for the Gardaí to arrive before moving the vehicles.",
+                correct: false,
+                answered: false
+            }
+        ]
+    },
+    {
+        id: 9,
+        question: "What should a driver do if involved in an incident where there is damage to property only?",
+        answers: [{
+                text: "It is only necessary to report the incident to the Gardaí within 10 days.",
+                correct: false,
+                answered: false
+            },
+            {
+                text: "There is no need to report the incident if there is no damage to the vehicle.",
+                correct: false,
+                answered: false
+            },
+            {
+                text: "Report the incident both to the property owner and the Gardaí within 48 hours",
+                correct: true,
+                answered: false
+            },
+            {
+                text: "Run away.",
+                correct: false,
+                answered: false
+            }
+        ]
+    },
+    {
+        id: 10,
+        question: "What should a driver do where a person has been injured in a collision?",
+        answers: [{
+                text: "Move the victim to the side of the road.",
+                correct: false,
+                answered: false
+            },
+            {
+                text: "Move the vehicles out of the way to avoid causing obstruction.",
+                correct: false,
+                answered: false
+            },
+            {
+                text: "Always move the victim and the vehicles.",
+                correct: false,
+                answered: false
+            },
+            {
+                text: "Move the victim only if there is a risk of fire or further injury.",
+                correct: true,
+                answered: false
+            },
+        ]
+    },
+]
