7c4b61c751b69eb929a660d1843fa1d055024ec3
Gary Barrett <itsgaz@gmail.com>
2021-04-28 21:21:28 +0000

    js: add success notice when contact form sends emails

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -7,11 +7,21 @@ function sendMail(contactForm) {
     .then(
         function(response) {
             console.log("SUCCESS", response); //Check log to make sure email is sent
+            let targetDiv = document.getElementById("formFeedback");
+            let successMessage = "Your message has been sent";
+            targetDiv.className = "alert-success";
+            contactForm.name.value = "";
+            contactForm.emailaddress.value = "";
+            contactForm.message.value = "";
+            targetDiv.innerHTML = successMessage;
+            setTimeout(() => {
+                targetDiv.innerHTML = "";
+            }, 5000);
         },
         function(error) {
             console.log("FAILED", error);
         }
     );
-    document.getElementById("contact-form").reset();
+    document.getElementById("contact-form").reset(); // Clear text from form after submission
     return false;  // Block from loading a new page
 }
