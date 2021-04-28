8872f802ec2d0b90a4cb8a1cd657e705d32fbc02
Gary Barrett <itsgaz@gmail.com>
2021-04-28 02:01:37 +0000

    js: setup email.js with contact form

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,16 @@
+function sendMail(contactForm) {
+    emailjs.send("drivingschool", "drivingschool", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.message.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response); //Check log to make sure email is sent
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // Block from loading a new page
+}
